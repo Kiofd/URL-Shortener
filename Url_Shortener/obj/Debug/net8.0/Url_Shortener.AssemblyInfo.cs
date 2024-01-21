3edc592617d161d94a6b201dc760a69f97886dc5
Maksym <max.sheremeta228@gmail.com>
2024-01-21 22:40:12 +0200

    Add gitignore

--- Url_Shortener/obj/Debug/net8.0/Url_Shortener.AssemblyInfo.cs
+++ Url_Shortener/obj/Debug/net8.0/Url_Shortener.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Url_Shortener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2193179b37df19f8c9ee949262a0256ff95b7b68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Url_Shortener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Url_Shortener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
